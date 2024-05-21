cebbacd675ad10d1b69b0e39248e33f3bbb30995
KEEN <kennicmwimu@gmail.com>
2024-05-21 09:11:06 +0300

    created pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 function App() {
   return (
-    <h1 className='text-2xl bg-red-500'>hello</h1>
+    <>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offer" element={<Offer/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
-export default App;
+export default App; 
