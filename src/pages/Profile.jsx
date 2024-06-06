5c74fd1f182ea0be5056ff56ab690cd84c5ca9f4
KEN <kennicmwimu@gmail.com>
2024-06-06 09:11:31 +0300

    added listing component in the profile and also created the listing item component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,21 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
-import { doc, updateDoc } from "firebase/firestore";
+import Spinner from "../Components/Spinner";
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { FcHome } from "react-icons/fc";
 import { Link } from "react-router-dom";
+import ListingItem from "../Components/ListingItem";
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
@@ -14,6 +24,8 @@ export default function Profile() {
     email: auth.currentUser.email,
   });
   const { name, email } = FormData;
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   function onLogout() {
     auth.signOut();
     navigate("/");
@@ -46,7 +58,28 @@ export default function Profile() {
       console.log(error);
     }
   }
-
+  useEffect(() => {
+    async function fetchUserListings() {
+      setLoading(true);
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("useRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -95,14 +128,40 @@ export default function Profile() {
               </p>
             </div>
           </form>
-          <button type="submit" className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded hover:bg-blue-700 shadow-md transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 ">
-            <Link className="flex justify-center items-center" to="/create-listing">
-              <FcHome className="mr-2 text-lg bg-red-200 rounded-full border-2 "/>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded hover:bg-blue-700 shadow-md transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 "
+          >
+            <Link
+              className="flex justify-center items-center"
+              to="/create-listing"
+            >
+              <FcHome className="mr-2 text-lg bg-red-200 rounded-full border-2 " />
               sell or rent your home
             </Link>
           </button>
         </div>
       </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {loading ? (
+          <Spinner />
+        ) : listings ? (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>No listings found</p>
+        )}
+      </div>
     </>
   );
 }
