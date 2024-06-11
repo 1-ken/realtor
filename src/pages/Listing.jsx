69456b7e1157a17ef1e7a312729a07c9fb70d08a
KEN <kennicmwimu@gmail.com>
2024-06-11 23:37:05 +0300

    add share functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router";
 import { db } from "../firebase";
 import Spinner from "../Components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FaShare } from "react-icons/fa";
 import SwiperCore, {
   EffectFade,
   Autoplay,
@@ -11,12 +12,14 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
+import { FaSackDollar } from "react-icons/fa6";
 
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   useEffect(() => {
     async function fetchListing() {
       const docRef = doc(db, "listings", params.listingId);
@@ -48,12 +51,25 @@ export default function Listing() {
               className="relative w-full overflow-hidden h-[300px]"
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                backgroundSize:"cover"
+                backgroundSize: "cover",
               }}
             ></div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-gray-400 rounded-full w-12 h-12 flex justify-center items-center "
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link copied</p>}
     </main>
   );
 }
